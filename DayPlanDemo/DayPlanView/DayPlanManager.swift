61a41c2fd7b2531d245c2f75a33cebf98f3bbf5c
Igor Grankin <grankin.igor.n1@gmail.com>
2019-12-16 17:35:18 +0300

    added: events on DayPlanView

---
+++ DayPlanDemo/DayPlanView/DayPlanManager.swift
@@ -0,0 +1,13 @@
+//
+//  DayPlanManager.swift
+//  DayPlanDemo
+//
+//  Created by admin on 16/12/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+
+class DayPlanManager {
+    
+}
--- DayPlanDemo/DayPlanView/DayPlanView.swift
+++ DayPlanDemo/DayPlanView/DayPlanView.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
-struct Calendar {
-    var length: CGFloat
+struct Event {
+    var startTime: Date
+    var endTime: Date
 }
+
+struct Calendar {
+    var events: [Event]
+}
+
 
 class DayPlanView: UIView {
 
@@ -25,6 +31,8 @@ class DayPlanView: UIView {
     
     private var hourWidth: CGFloat = UIScreen.main.bounds.width / 3
     private var minSectionHeight: CGFloat = 250
+    
+    var backgroundEventHeight: CGFloat = 70
     private var contentSize: CGSize = CGSize.zero
     private var totalHeight: CGFloat = 0
     private var currentMembers: [Calendar] = [Calendar]()
@@ -34,6 +42,13 @@ class DayPlanView: UIView {
     private var timelineView: UIView = UIView()
     
     private var shapeLayersArray: [CAShapeLayer] = [CAShapeLayer]()
+    
+    private var events: [Event] = [Event]()
+    
+    //EventView
+    let eventViewHeight = 50
+    let eventViewMinWidth = 70
+    let eventOffsetHeight: CGFloat = 10
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -55,31 +70,30 @@ class DayPlanView: UIView {
         self.addSubview(scrollview)
         self.addSubview(timelineView)
         currentMembers.removeAll()
-        let firstCalendar = Calendar(length: 70)
-        let secondCalendar = Calendar(length: 140)
-        let thirdCalendar = Calendar(length: 100)
+        events.removeAll()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 02:00")!, endTime: formatter.date(from: "2016/10/08 05:10")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 03:10")!, endTime: formatter.date(from: "2016/10/08 08:10")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 05:11")!, endTime: formatter.date(from: "2016/10/08 08:10")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 05:11")!, endTime: formatter.date(from: "2016/10/08 08:10")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 03:11")!, endTime: formatter.date(from: "2016/10/08 07:23")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 02:00")!, endTime: formatter.date(from: "2016/10/08 03:09")!))
+        events.append(Event(startTime: formatter.date(from: "2016/10/08 02:00")!, endTime: formatter.date(from: "2016/10/08 04:23")!))
+        
+        let firstCalendar = Calendar(events: events)
         
         currentMembers.append(firstCalendar)
-        currentMembers.append(secondCalendar)
-        currentMembers.append(thirdCalendar)
-        currentMembers.append(Calendar(length: 70))
-        currentMembers.append(Calendar(length: 70))
-        currentMembers.append(Calendar(length: 70))
-        currentMembers.append(Calendar(length: 70))
-        currentMembers.append(Calendar(length: 70))
-        currentMembers.append(Calendar(length: 70))
         
-        for currentMember in currentMembers {
-            totalHeight += currentMember.length
-        }
+        
+//        for currentMember in currentMembers {
+//            totalHeight += backgroundEventHeight
+//        }
     }
     
     override func layoutSubviews() {
-        for layer in shapeLayersArray {
-            layer.removeFromSuperlayer()
-        }
-        shapeLayersArray.removeAll()
         
+        totalHeight = self.frame.size.height
         timelineView.frame = CGRect(x: 0, y: 0, width: 24 * hourWidth, height: timelineViewHeight)
         timelineView.backgroundColor = .red
         scrollview.frame = CGRect(x: 0, y: timelineViewHeight, width: self.frame.size.width, height: self.frame.size.height)
@@ -88,13 +102,75 @@ class DayPlanView: UIView {
         scrollview.backgroundColor = .clear
         scrollview.delegate = self
     
+        
+        
+        drawLayers()
+        
+        showEventsViews()
+        super.layoutSubviews()
+    }
+    
+    func drawLayers() {
+        for layer in shapeLayersArray {
+            layer.removeFromSuperlayer()
+        }
+        shapeLayersArray.removeAll()
+        
         drawMemberLines()
         drawHoursLines()
-        //TODO: make method
         drawHoursLabels()
-        
-        
-        super.layoutSubviews()
+    }
+    
+    func showEventsViews() {
+        //TODO: remove subviews
+        for subview in scrollview.subviews {
+            if !subview.isKind(of: NSClassFromString("_UIScrollViewScrollIndicator")!) {
+                subview.removeFromSuperview()
+            }
+        }
+        for event in events {
+            let eventView = UIView()
+            eventView.backgroundColor = .random()
+            let timeDiff = (event.endTime.timeIntervalSince1970 - event.startTime.timeIntervalSince1970) / 3600
+            let tempEventViewWidth = CGFloat(timeDiff) * hourWidth
+            let eventViewWidth = tempEventViewWidth < hourWidth ? hourWidth : tempEventViewWidth
+            //Point calculation
+            /// x coordinate calculation
+            let hour = NSCalendar.current.component(.hour, from: event.startTime)
+            let minute = NSCalendar.current.component(.minute, from: event.startTime)
+            let xCoord = CGFloat(hour) * hourWidth + CGFloat(minute) / 60 * hourWidth
+            ///y coordinate calculation
+            var tempRect = CGRect(x: xCoord, y: eventOffsetHeight, width: CGFloat(eventViewWidth), height: CGFloat(eventViewHeight))
+            var didFoundYCoord = false
+            var shouldUpdateScrollViewHeight = false
+            while (!didFoundYCoord) {
+                for currentEventView in scrollview.subviews {
+                    if tempRect.intersects(currentEventView.frame) {
+                        tempRect.origin.y += backgroundEventHeight
+                        didFoundYCoord = false
+                        if tempRect.origin.y > totalHeight  {
+                            shouldUpdateScrollViewHeight = true
+                        }
+                        break
+                    }
+                    didFoundYCoord = true
+                }
+            }
+            if shouldUpdateScrollViewHeight {
+                totalHeight += backgroundEventHeight
+                scrollview.contentSize.height = totalHeight
+                drawLayers()
+            }
+            eventView.frame = tempRect
+            
+            let timeLabel = UILabel()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH:mm"
+            timeLabel.text = "\(dateFormatter.string(from: event.startTime))-\(dateFormatter.string(from: event.endTime))"
+            timeLabel.frame = eventView.bounds
+            eventView.addSubview(timeLabel)
+            scrollview.addSubview(eventView)
+        }
     }
     
     func drawHoursLabels() {
@@ -109,6 +185,7 @@ class DayPlanView: UIView {
     func drawHoursLines() {
         for currentHour in 0..<24 {
             let dashedHourLine = CAShapeLayer()
+            dashedHourLine.zPosition = scrollview.layer.zPosition-1
             shapeLayersArray.append(dashedHourLine)
             dashedHourLine.strokeColor = UIColor.black.cgColor
             dashedHourLine.lineWidth = 1
@@ -123,13 +200,15 @@ class DayPlanView: UIView {
     
     func drawMemberLines() {
         var lastOffset: CGFloat = 0
-        for currentMember in 0..<currentMembers.count {
+        let backgroundNumbers = Int(totalHeight / backgroundEventHeight)
+        for currentBackground in 0..<backgroundNumbers {
             let dashedHourLine = CAShapeLayer()
+            dashedHourLine.zPosition = scrollview.layer.zPosition-1
             shapeLayersArray.append(dashedHourLine)
-            let currentColor = currentMember % 2 == 0 ? UIColor.white : UIColor.lightGray
+            let currentColor = currentBackground % 2 == 0 ? UIColor.white : UIColor.lightGray
             dashedHourLine.fillColor = currentColor.cgColor
             
-            let length = currentMembers[currentMember].length
+            let length = backgroundEventHeight
             let coordY:CGFloat = lastOffset + length
             let bezierPath = UIBezierPath()
             bezierPath.move(to: CGPoint(x: 0, y: lastOffset))
@@ -137,7 +216,6 @@ class DayPlanView: UIView {
             bezierPath.addLine(to: CGPoint(x: contentSize.width, y: coordY))
             bezierPath.addLine(to: CGPoint(x: contentSize.width, y: lastOffset))
             bezierPath.close()
-            print("member N \(currentMember) bezier \(bezierPath)")
             lastOffset = coordY
             
             dashedHourLine.path = bezierPath.cgPath
@@ -159,5 +237,20 @@ extension DayPlanView : UIScrollViewDelegate{
         timelineView.frame.origin.x += DayPlanView.previousOffset - scrollview.contentOffset.x
         DayPlanView.previousOffset = scrollview.contentOffset.x
        
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
     }
 }
